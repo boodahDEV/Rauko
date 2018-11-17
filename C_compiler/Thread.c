7c6c844a5ea669f4624b11c858d6ec040e38334a
@boodah <boodah21@protonmail.com>
2018-11-17 08:10:21 -0500

    Se trata de unir poco a poco.

--- C_compiler/Thread.c
+++ C_compiler/Thread.c
@@ -6,21 +6,33 @@
 
 void *saludo(void * args){
 char *cadena="hilo1";
-    
-    for(int i = 0; i < strlen(cadena); i++)
+char * cadena2= (char *)args;
+ /*   for(int i = 0; i < strlen(cadena); i++)
     {
         fflush(stdout);
         printf("%c",cadena[i]);
         sleep(1);
+    }*/
+    printf("\n");
+    for(int i = 0; i < strlen(cadena2); i++)
+    {
+        fflush(stdout);
+        printf("%c",cadena2[i]);
+        sleep(1);
     }
+    
     printf("\n");
 }
 main(int argc, char const *argv[])
 {
-    pthread_t hilo1;
-    pthread_create(&hilo1,NULL,saludo,NULL); // La Funcion pthread_create tiene 4 argumentos, el primero es la referencia(&) del hilo, el segundo lo pongo nulo
-                                                         // el tercero es la funcion que ejecutara ese hilo, y finalmente el 4 le paso valor NULL
-    printf("Hola mundo\n");
-    pthread_join(hilo1, NULL);
+    char *cadena = "hola mundo";
+   // pthread_t hilo1;
+    pthread_t hilo2;
+   // pthread_create(&hilo1,NULL,saludo,NULL); // La Funcion pthread_create tiene 4 argumentos, el primero es la referencia(&) del hilo, el segundo lo pongo nulo
+                                             // el tercero es la funcion que ejecutara ese hilo, y finalmente el 4 le paso valor NULL 
+    pthread_create(&hilo2,NULL,saludo,(void *)cadena);
+    //printf("Hola mundo\n");
+   // pthread_join(hilo1, NULL);
+    pthread_join(hilo2, NULL);
     return 0;
 }
