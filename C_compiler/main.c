d3b0d018901e6408dfdc19951bc9947e9137fbf4
@boodah <boodah21@protonmail.com>
2018-11-18 15:51:52 -0500

    Se reorganiza la estructura principal.

--- C_compiler/main.c
+++ C_compiler/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+//---=== prototipos===---
+void Busca();
 
-int main(){
-    printf("Hola mundo");
+//---=== MAIN ===---
+void main(){
 
-    return 0;
+Busca(); // busca en el archivo!
 }
+//---=== END MAIN ===---
+
+
+void Busca(){
+    FILE *archivo;
+	char *texto;
+    char mucho[10];
+	archivo = fopen("data.dll","r");
+	
+	if (archivo == NULL){
+            printf("\nError de apertura del archivo. \n\n");
+        }else{
+            texto = fgets(mucho, 10, archivo);
+			printf("%s", mucho);
+        }
+        fclose(archivo);
+} //---=== END BUSCA ===---
