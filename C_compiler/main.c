a538c8ea662380c5ccc1d52979735655f7e7710d
@boodah <boodah21@protonmail.com>
2018-11-18 21:22:52 -0500

    Se trabaja en la busqueda de comandos por regex. Ajustes leves.

--- C_compiler/main.c
+++ C_compiler/main.c
@@ -3,48 +3,83 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <string.h>
+#include <regex.h>
 
+#define MAX_COMANDOS 2
+#define TAM_COMANDO 3
+#define CANT_COM_CAP 10
 //---=== prototipos===---
 void open(char *,char *);
-
+void cargaComandos(char *,char [][TAM_COMANDO]);
 
 //---=== MAIN ===---
 void main(){
-	char texto[10]; //---=== DATA DEL ARCHIVO!
+	char t_comando[CANT_COM_CAP]; //---=== DATA DEL ARCHIVO!
     char url[]= "./dll/data_1.dll";
-    int num;
- open(url,texto);// busca en el archivo!
- 
- for(int i = 0; i < 10; i++)
- {
-     printf("%c",texto[i]);
- }
+   static char m_comando[MAX_COMANDOS][TAM_COMANDO]={{'\0'}};
 
- sscanf(texto, "%d", &num);
- printf("\nnumero: %d\n",num);
+ open(url,t_comando);// busca en el archivo!
+ cargaComandos(t_comando,m_comando);
  
+ /*for(int i = 0; i < 10; i++)
+ {
+     printf("%c [%d]\n",texto[i],i);
+ }
+*/
+
+printf("\n\n");
+for(int i = 0;i<MAX_COMANDOS; i++)
+{    
+
+    for(int j = 0;j<TAM_COMANDO; j++)
+    {
+        printf("%c",m_comando[i][j]);
+    }
+    printf("\n");
+}
+
+
 }
 //---=== END MAIN ===---
 
 
-void open(char *url,char *texto){
+void open(char *url,char *t_comando){
     FILE *archivo;
-    int num;
+    int comando[2];
 	archivo = fopen(url,"r");
 	
 	if (archivo == NULL){
             printf("\nError de apertura del archivo. \n\n");
         }else{
-            //texto = fgets(mucho, 10, archivo);
-     
-            for(int i = 0; i < 10; i++)
+            //texto = fgets(mucho, 10, archivo)
+            for(int i = 0; i < CANT_COM_CAP; i++)
             {
-
-                fscanf(archivo,"%c", &texto[i]);
-                
-              //  fgetc(archivo);
+                fscanf(archivo,"%c", &t_comando[i]);
             }      
         }
         fclose(archivo);
 
+    sscanf(t_comando, "%d %d", &comando[0],&comando[1]);    //---=== ESTA FORMA DE DETERMINAR EL COMANDO QUEDARA OBSOLETA AL TENER MAS COMANDOS DEFINIDOS EN EL DLL 
+    printf("El numero: %c%c\n",comando[0],comando[1]);      //
 } //---=== END BUSCA ===---
+
+
+//---=== CARGA LOS COMANDOS A UNA MATRIZ ESTATICA PARA TENERLO EN MEMORIA ===---
+void cargaComandos(char *t_comando,char m_comando[][TAM_COMANDO]){
+    int p_rescom = 0;
+    int p_cancom = 0;
+
+    for(int i = 0; i<CANT_COM_CAP; i++){
+        if(t_comando[i] != '\n'&& t_comando[i] ){
+            m_comando[p_cancom][p_rescom] = t_comando[i];
+            p_rescom++;
+        }
+        else
+        {
+            m_comando[p_cancom][p_rescom]= '\0';
+            p_rescom =0;
+            p_cancom++;
+        }
+    }
+}// END METODO CARGACOMANDOS
+
