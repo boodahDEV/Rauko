7c8ebbf1cebf6008b2bd47e2ef5207d2d97f3df9
@boodah <boodah21@protonmail.com>
2018-11-29 15:36:42 -0500

    Se completa la busqueda correctamente por comandos.

--- C_compiler/main.c
+++ C_compiler/main.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <string.h>
-#include <regex.h>
+//#include <regex> //
 
 #define MAX_DATO_IN 50
 
@@ -18,20 +18,21 @@ nod * memoria (nod *);
 nod * creainicio (nod*, FILE *,FILE *);
 void imprimir(nod *);
 nod * busca(nod *, char *);
-void openDocument(char *, FILE *);
+void openDocument(char *, FILE *, nod*);
 
 void main (){
 		nod *p;
 		FILE *archivo1,*archivo2,*archivo;
-		char texto[50];
+		char texto_entrada[50];
 
-		scanf("%[^\n]",texto);
-		openDocument(texto,archivo);
-	//	p=creainicio(p,archivo1,archivo2);
-	//	imprimir(p);
-	//	printf("\n\nDame el comando: ");scanf("%[^\n]",texto);
-	//	busca(p,texto);
-		printf("Listo...\n");
+    system("clear");
+	scanf("%[^\n]",texto_entrada);
+		//char entrada_secm[50];
+	p=creainicio(p,archivo1,archivo2);
+	openDocument(texto_entrada,archivo,p);
+	//imprimir(p);
+	//printf("\n\nDame el comando: ");scanf("%[^\n]",entrada_secm);
+	//busca(p,entrada_secm);
 }//---=== END MAIN()
 
 nod * memoria (nod * x){
@@ -85,51 +86,60 @@ nod * creainicio (nod *p, FILE *archivo1,FILE *archivo2){
 nod * busca(nod *p, char *texto){
 	nod *q;
 	q=p;
-	int band = 0;
-		/*	while(q!=NULL && q->com_name!=texto){
-				q=q->liga;
-			}
-			if(q==NULL){
-				printf("EL elemento no se encuentra en la lista!\n");
-			}else{
-				printf("Encontrado en la lista!.\n");
-			}
-			*/
+	int band = 0,j=0;
+
  while(q != NULL && !band){
     if (strcmp(q->com_name,texto)==0) {
         band = 1; // si esta en la lista el texto 
-    } else {
-        q=q->liga;// si no esta pasa al siguiente nodo
-    }
+    	} else {
+      	 	 q=q->liga;// si no esta pasa al siguiente nodo
+    	}
  }//end while
 	if(band){
-   		 printf("\n\n Numero %s esta en la lista", texto);
-	} else {
-   		 printf("\n\n El numero %s no esta en la lista", texto);
-	}
+   		 printf("\n\n El elemento %s esta en la lista", texto);
+		} else {
+   		 printf("\n\n El elemento %s no esta en la lista", texto);
+		}
 }//termina la funcion busca
 
 
-void openDocument(char *texto, FILE *archivo){
-	char temp[MAX_DATO_IN];
+void openDocument(char *texto, FILE *archivo,nod *p){
+	char temp[MAX_DATO_IN]; //---===  RECUPERA EL TEXTO DEL ARCHIVO DE COMPILACION
 	char delim[] = " "; // se que esto podria ser un simple char sin arreglo pero el strtok requiere que el delimitador sea un arreglo
-    char *texto_divido = NULL;
-	//char []
-	archivo = fopen(texto,"r");
-if (archivo== NULL){
+    char *texto_divido; // strtok
+	char palabra[1];
+
+	archivo = fopen(texto,"r"); //---=== abre el documento de la linea de comando
+
+ if (archivo== NULL){
             printf("\nError en el analicis. \n\n");
         }else{
           while (feof(archivo) == 0){
-			  fgets(temp,MAX_DATO_IN,archivo);
+			  fgets(temp,MAX_DATO_IN,archivo);//---=== TEMP[] TIENE LA DATA DEL ARCHIVO
+			  
 		  }
+	for(int i = 0; i < (int)strlen(temp); i++){
+		if(temp[i]==' '){
+			strncpy(palabra,temp,2);
+			//printf("\t\t[%s][%d]",palabra,((int)strlen(palabra)));
+			busca(p,palabra);
+			break;
 		}
-
-    texto_divido = strtok( temp, delim);	// strtok divide una cadena de texto de acuerdo a un delimitador, en este caso le dije el delimitador 32 y lo guarde en separador
+			
+	}
+		}
+	//strncat(palabra,temp,2);
+	
+	
+ /*   texto_divido= strtok( temp, delim);	// strtok divide una cadena de texto de acuerdo a un delimitador, en este caso le dije el delimitador 32 y lo guarde en separador
 
     while( texto_divido != NULL ) {
         printf( "Trozo : %s \n", texto_divido);
-        texto_divido = strtok( NULL, delim); //la funcion strtok devuelve un puntero al primer token encontrado en la cadena.
-										     // Se devuelve un puntero nulo si no quedan tokens para recuperar.
+        
+        texto_divido= strtok( NULL, delim); //la funcion strtok devuelve un puntero al primer token encontrado en la cadena.
+		//strcpy(entrada_secm[i],&texto_divido);
+        i++;								 // Se devuelve un puntero nulo si no quedan tokens para recuperar.
     }
 	//printf("[ %s ]",temp);
+*/
 }
