36a17b357f2d5cdf08ab62b1b5554c916cf19455
@boodah <boodah21@protonmail.com>
2018-12-26 16:54:51 -0500

    Se vuelve a su estado inicial Thread.c

--- programacion_superior/Thread.c
+++ programacion_superior/Thread.c
@@ -29,6 +29,7 @@ pthread_t hilo3;
         printf("%c",cadena2[i]);
         usleep(20000);
         if(cadena2[i]== '\n'){
+
              pthread_create(&hilo3,NULL,unido,(void *)cadena);
              pthread_join(hilo3, NULL);
         }
@@ -46,25 +47,24 @@ void *ejecuta(void *x){
 
 void main(int argc, char const *argv[])
 {
-    char *cadena = "hola mundo, esto es una prueba de la velocidad de ejecucion a base de directivas!";
-   // pthread_t hilo1;
+    char *cadena = "hola mundo, esto es una prueba de la velocidad de ejecucion a base de directivas!\n";
+    pthread_t hilo1;
    const int tam =(int) strlen(cadena);
    pthread_t hilos[tam];
     pthread_t hilo2;
-   // pthread_create(&hilo1,NULL,saludo,NULL); // La Funcion pthread_create tiene 4 argumentos, el primero es la referencia(&) del hilo, el segundo lo pongo nulo
+    pthread_create(&hilo1,NULL,saludo,(void *)cadena); // La Funcion pthread_create tiene 4 argumentos, el primero es la referencia(&) del hilo, el segundo lo pongo nulo
                                              // el tercero es la funcion que ejecutara ese hilo, y finalmente el 4 le paso valor NULL 
   //  pthread_create(&hilo2,NULL,saludo,(void *)cadena);
-   printf("tengo t: %d\n",tam);
+/*    printf("tengo t: %d\n",tam);
     printf("tengo a: %d\n",(int)sizeof(hilos));
 
     for(int i = 0; i < tam; i++){
-     
       pthread_create(&hilos[i],NULL,ejecuta,&cadena[i]);
-    }
+    } */
         
         //pthread_join(hilos[0], NULL);
       
     
-   // pthread_join(hilo1, NULL); obligatorio poner el join ya que el hilo main siempre muere antes que se ejecuta
+    pthread_join(hilo1, NULL);// obligatorio poner el join ya que el hilo main siempre muere antes que se ejecuta
     //pthread_join(hilo2, NULL);
 }
