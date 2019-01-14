6c478e3d333027c174339229bd9addcf772e2288
@boodah <boodah21@protonmail.com>
2019-01-14 11:09:25 -0500

    Manipulacion de tiempo de ejecucion de un pthread en P(hijo).

--- src/mainV1.5.0.c
+++ src/mainV1.5.0.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <pthread.h>
 
 #ifdef UNIX
+    #include <unistd.h>
+    #include <sys/wait.h>
     #define sys "UNIX"
-    #define TYPE_SYSTEM_EXEC(sys) ((sys == "UNIX") ? printf("Ejecutando en sistema basado en GNU/LINUX.\n") : printf("Ejecuntando en sistema basado en Windows/Microsoft.\n"))
+    #define TYPE_SYSTEM_EXEC(sys) ((sys == "UNIX") ? printf("Ejecutando en sistema basado en GNU/LINUX.\n\n\n") : printf("Ejecuntando en sistema basado en Windows/Microsoft.\n\n\n"))
 #else
     #define sys "WIN"
-    #define TYPE_SYSTEM_EXEC(sys) ((sys == "UNIX") ? printf("Ejecutando en sistema basado en GNU/LINUX.\n") : printf("Ejecuntando en sistema basado en Windows/Microsoft.\n"))
+    #define TYPE_SYSTEM_EXEC(sys) ((sys == "UNIX") ? printf("Ejecutando en sistema basado en GNU/LINUX.\n\n\n") : printf("Ejecuntando en sistema basado en Windows/Microsoft.\n\n\n"))
 #endif
+void *lexan(void *);
 
-void main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
     char texto_entrada[50];
     static pid_t RESULT_FORK;
-    // static pthread_t LEX_THREAD_MAIN; // hilo de ejecucion del analicis lexico!
-    TYPE_SYSTEM_EXEC(sys);
+    pthread_t LEX_THREAD_MAIN; // hilo de ejecucion del analicis lexico!
+    TYPE_SYSTEM_EXEC(sys);     // tipo de sistema en ejecucion.
 
     //======= TODO APARTIR DE AQUI SE DUPLICARA EN EL PROCESO HIJO, TOMAR EN CUENTA AL TRATAR DE MAXIMIZAR EL CODIGO =======//
     RESULT_FORK = fork();
     if (RESULT_FORK == -1)
     {
-        printf("Error al crear procesos!"); //IDEAR UNA LIBRERIA PARA LA MANIPULACION DE ERRORES!
+        printf("ERROR, NO HAY MEMORIA SUFICIENTE PARA EJECUTAR LA ACCION.\n\n\n"); //IDEAR UNA LIBRERIA PARA LA MANIPULACION DE ERRORES!
+        exit(EXIT_FAILURE);
     }
     else if (RESULT_FORK == 0)
     {
-        // pthread_create(&LEX_THREAD_MAIN,NULL,lexan,(void *)cadena);
+        pthread_create(&LEX_THREAD_MAIN, NULL, lexan, (void *)sys);
         printf("\n\tID del HIJO: %d y el papa es: %d\n", getpid(), getppid());
+        pthread_join(LEX_THREAD_MAIN, NULL); //ESTE PROCESOS ESPERA A QUE EL HILO CULMINE
+        //pthread_detach(LEX_THREAD_MAIN);
     }
     else
     {
         printf("\n\tID del PADRE: %d y el padre es: %d\n", getpid(), getppid());
+        pthread_join(LEX_THREAD_MAIN,NULL);
     } // end else del padre!
     //======= NO COLOCAR NADA DESPUES DE AQUI, SI CONSIDERAS QUE EL PROCESO PADRE EJECUTE ALGO INDEPENDIENTE, DEBE SER DENTRO DEL IF, Y LO MISMO EN EL HIJO. ======/
+    pthread_join(LEX_THREAD_MAIN,NULL);
+    exit(EXIT_SUCCESS);
 } //end main
+//-1 para firnalizar por algun error en especifico
+
+void *lexan(void *args){ // para pasar multiples parametros a esta funciones de hilo, debe ser por estructuras!
+    char * t_sys= (char *)args;
+    int i = 0;
+ while(i!=15){ //CICLO DE PRUEBA
+     printf("--#%s#--\n",t_sys);
+     usleep(500000);
+     i++;
+ }
+}
