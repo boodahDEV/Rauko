8645831ca81cf20adb6cd6bf3652b3b8c02bb159
@boodah <boodah21@protonmail.com>
2019-01-19 11:24:36 -0500

    Se trata la comunicacion entre los proceso.

--- src/mainV1.5.0.c
+++ src/mainV1.5.0.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#ifdef UNIX
+#if UNIX
     #include <unistd.h>
     #include <sys/wait.h>
     #define sys "UNIX"
@@ -19,28 +19,26 @@ int main(int argc, char const *argv[])
     static pid_t RESULT_FORK;
     pthread_t LEX_THREAD_MAIN; // hilo de ejecucion del analicis lexico!
     TYPE_SYSTEM_EXEC(sys);     // tipo de sistema en ejecucion.
-
     //======= TODO APARTIR DE AQUI SE DUPLICARA EN EL PROCESO HIJO, TOMAR EN CUENTA AL TRATAR DE MAXIMIZAR EL CODIGO =======//
     switch (RESULT_FORK = fork())
     {
     case -1:
     {
-        printf("ERROR, NO HAY MEMORIA SUFICIENTE PARA EJECUTAR LA ACCION.\n\n\n"); //IDEAR UNA LIBRERIA PARA LA MANIPULACION DE ERRORES!
         exit(EXIT_FAILURE);
     }
     case 0:
     {
+        printf("Hola hijo! %d\n",getpid());
         pthread_create(&LEX_THREAD_MAIN, NULL, lexan, (void *)sys);
-        printf("\n\tID del HIJO: %d y el papa es: %d\n", getpid(), getppid());
         pthread_join(LEX_THREAD_MAIN, NULL); //ESTE PROCESOS ESPERA A QUE EL HILO CULMINE
         //pthread_detach(LEX_THREAD_MAIN);
         exit(EXIT_SUCCESS);
     }
     default:
     {
-        printf("\n\tID del PADRE: %d y el padre es: %d\n", getpid(), getppid());
-        pthread_join(LEX_THREAD_MAIN, NULL);
-        exit(EXIT_SUCCESS);
+        printf("Hola papa! %d\n",getpid());
+     //  pthread_join(LEX_THREAD_MAIN, NULL);
+     //   exit(EXIT_SUCCESS);
     }
     } //end switch
     //======= NO COLOCAR NADA DESPUES DE AQUI, SI CONSIDERAS QUE EL PROCESO PADRE EJECUTE ALGO INDEPENDIENTE, DEBE SER DENTRO DEL IF, Y LO MISMO EN EL HIJO. ======/
@@ -57,4 +55,5 @@ void *lexan(void *args)
         usleep(500000);
         i++;
     }
-}
+    printf("\nFin del hilo lexan!\n");
+}//fin analicis lexico
